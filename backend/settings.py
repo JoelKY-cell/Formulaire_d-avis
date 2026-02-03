ca47521dc4fb6a72ebf5730f2ace91de969204a1
JoelKY-cell <joelkyras3@gmail.com>
2026-02-03 22:39:18 +0000

    Mise à jour des paramètres de configuration et suppression de la base de données obsolète.

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATIC_URL = '/django_static/'
 STATIC_ROOT = BASE_DIR / "static"
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
